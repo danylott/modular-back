aa27a08252af6f69b5928e80fa52f6cfe62e5cf2
Aleksandr Uspenskyi <alex@springsapps.com>
2020-06-15 17:11:01 +0300

    restructure + file uploads

--- models/class.js
+++ models/class.js
@@ -1,8 +1,36 @@
 const mongoose = require("mongoose")
+const { gql } = require("apollo-server")
 
-module.exports = mongoose.model("class", {
-  name: { type: String, unique: true },
-  make: String,
-  status: { type: String, default: "collecting" },
-  markup: mongoose.Schema.Types.Mixed,
-})
+module.exports = {
+  Class: mongoose.model("class", {
+    name: { type: String, unique: true },
+    make: String,
+    status: { type: String, default: "collecting" },
+    markup: mongoose.Schema.Types.Mixed,
+  }),
+  classDefs: gql`
+    # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+    type Class {
+      name: String!
+      make: String
+      status: String
+      markup: [ClassMarkup]
+    }
+
+    type ClassMarkup {
+      field: String
+      x: Int
+      y: Int
+      w: Int
+      h: Int
+    }
+
+    input ClassMarkupInput {
+      field: String
+      x: Int
+      y: Int
+      w: Int
+      h: Int
+    }
+  `,
+}
