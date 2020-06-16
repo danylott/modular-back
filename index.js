9e9dfa211ce52eb39cbf2e3ee969a1d185f7890d
Zimovets_Sergey <zimovets_s@ukr.net>
2020-06-16 13:26:19 +0300

    Model data type changed

--- index.js
+++ index.js
@@ -1,68 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server")
 const mongoose = require("mongoose")
-const Class = require("./models/class")
 
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-  type Class {
-    name: String
-    make: String
-    status: String
-    markup: [ClassMarkup]
-  }
-
-  type ClassMarkup {
-    field: String
-    x: Float
-    y: Float
-    w: Float
-    h: Float
-  }
-
-  type Query {
-    classes: [Class]
-  }
-
-  input ClassMarkupInput {
-    field: String
-    x: Float
-    y: Float
-    w: Float
-    h: Float
-  }
-
-  type Mutation {
-    createClass(name: String, make: String): Class
-    updateClass(
-      name: String
-      make: String
-      status: String
-      markup: [ClassMarkupInput]
-    ): Class
-  }
-`
-
-const resolvers = {
-  Query: {
-    classes: () => {
-      return Class.find()
-    },
-  },
-  Mutation: {
-    createClass: async (_, data) => {
-      return Class.create(data)
-    },
-    updateClass: async (_, { name, make, status, markup }) => {
-      const cl = await Class.findOne({ name })
-      if (!cl) return null
-
-      if (make) cl.make = make
-      if (status) cl.status = status
-      if (markup) cl.markup = markup
-      return cl.save()
-    },
-  },
-}
 const { classDefs } = require("./models/class")
 const { queries, mutations, resolvers } = require("./graphql/class")
 
