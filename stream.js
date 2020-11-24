faf14ac7c9c88c5cabf064ed8c55f82aac84058a
danylott <d.tuytuyshkin@gmail.com>
2020-11-24 12:13:52 +0100

    compare previous recognition with next recognition and if > 95% matches - do not send one's more this info

--- stream.js
+++ stream.js
@@ -16,6 +16,47 @@ function sleep(s) {
   return new Promise((resolve) => setTimeout(resolve, s * 1000));
 }
 
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
+}
+
+function similarity(s1, s2) {
+  var longer = s1;
+  var shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  var longerLength = longer.length;
+  if (longerLength == 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
 let busy = false;
 let reader;
 
@@ -62,23 +103,36 @@ async function handleSingleImage(buffer) {
     });
 
     if (!!res.found && !!res.class) {
+      const previousRecognition = await Recognition.findOne({positionId}, {}, { sort: {'createdAt': -1}});
       const recognition = await Recognition.create({
         positionId,
         classId: res.class._id,
         score: res.score,
         recognized: { model: res.model, size: res.size, color: res.color },
       });
-      rabbitMq.publish('recognitions', {
-        positionId,
-        recognitionId: recognition._id,
-        barcode: null,
-        barcodeType: null,
-        image: buffer ? buffer.toString('base64') : null,
-        brand: res.class.make,
-        model: res.model,
-        size: res.size,
-        color: res.color,
-      });
+      let totalSimilatiry = 0;
+      if(!!previousRecognition) {
+        if (Object.keys(previousRecognition.recognized).length > 0) {
+          previousRecognition.recognized.size && (totalSimilatiry += similarity(previousRecognition.recognized.size, recognition.recognized.size));
+          previousRecognition.recognized.model && (totalSimilatiry += similarity(previousRecognition.recognized.model, recognition.recognized.model));
+          previousRecognition.recognized.color && (totalSimilatiry += similarity(previousRecognition.recognized.color, recognition.recognized.color));
+          totalSimilatiry /= 3;
+          console.log("totalSimilarity: ", totalSimilatiry);
+        }
+      }
+      if (totalSimilatiry <= 0.95) {
+        rabbitMq.publish('recognitions', {
+          positionId,
+          recognitionId: recognition._id,
+          barcode: null,
+          barcodeType: null,
+          image: buffer ? buffer.toString('base64') : null,
+          brand: res.class.make,
+          model: res.model,
+          size: res.size,
+          color: res.color,
+        });
+      }
     }
 
     await sleep(0.2);
